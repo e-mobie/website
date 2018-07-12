9a6948b03440f8bbdc7b59196b14d2c6543c4b52
ejay010 <ejay010@gmail.com>
2018-07-12 01:23:10 -0400

    dlkfdf

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -124,9 +124,36 @@ export default {
     onDecode: function(content) {
       this.pauseCamera = true
       swal({
+        type: 'info',
         title: 'Validating E-Code...',
         onOpen: () => {
           swal.showLoading()
+          let queryString = content.slice(1).split('&')
+          let queryObject = {}
+          queryString.forEach(function(pair) {
+            pair = pair.split('=');
+            queryObject[pair[0]] = decodeURIComponent(pair[1] || '');
+          })
+          let result = JSON.parse(JSON.stringify(queryObject))
+          this.$store.dispatch('LogToSlack', {
+            headline: 'QueryString',
+            log: result
+          })
+          this.qrCodeData = result
+          return axios.create({
+            withCredentials: true
+          }).post(process.env.VUE_APP_API_URL + '/purchaseOrder/' + this.$route.params.eventId + '/' +
+            this.qrCodeData.invoiceId + '/validate').then((response) => {
+            if (response.data.success) {
+              swal.hideLoading()
+            }
+          }).catch((error) => {
+            swal.showValidationError({
+              title: error.status,
+              text: error.message,
+              type: "error"
+            })
+          })
         },
         // preConfirm: () => {
         //   let queryString = content.slice(1).split('&')
