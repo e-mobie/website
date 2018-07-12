8645ae7435c5c4ca0e0c5ca63f41b4efcb70f6c6
ejay010 <ejay010@gmail.com>
2018-07-12 00:19:30 -0400

    jhjh

--- src/components/QrcodeReader.vue
+++ src/components/QrcodeReader.vue
@@ -126,7 +126,6 @@ export default {
       swal({
         type: 'info',
         title: 'Validating E-Code...',
-        text: 'One Moment...',
         onOpen: () => {
           swal.showloading()
         },
